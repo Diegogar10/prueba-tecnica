f0295151939b057d2dfb9b27bff57b847ca65eca
Diego Garcia <admin@dydautomatizacion.com>
2023-07-24 23:55:47 -0500

    project finished version BETA

---
+++ data-info.js
@@ -0,0 +1,174 @@
+export const data = [
+  {
+    age: 2022,
+    selected: true,
+    users:[
+      {
+        name: 'Rosa Inés Roa Pinilla 1',
+        url:'https://picsum.photos/200',
+        title: 'Fundación Desaparecidos Colombia Huellas de Cristal 1',
+        description: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
+        euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad
+        minim veniam, quis nostrud exerci tation Lorem ipsum dolor sit amet, consectetuer
+        adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+        aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation`
+      },
+      {
+        name: 'Rosa Inés Roa Pinilla 2',
+        url:'https://picsum.photos/300',
+        title: 'Fundación Desaparecidos Colombia Huellas de Cristal 2',
+        description: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
+        euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad
+        minim veniam, quis nostrud exerci tation Lorem ipsum dolor sit amet, consectetuer
+        adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+        aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation`
+      },
+      {
+        name: 'Rosa Inés Roa Pinilla 3',
+        url:'https://picsum.photos/400',
+        title: 'Fundación Desaparecidos Colombia Huellas de Cristal 3',
+        description: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
+        euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad
+        minim veniam, quis nostrud exerci tation Lorem ipsum dolor sit amet, consectetuer
+        adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+        aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation`
+      }
+    ]
+  },
+  {
+    age: 2021,
+    users:[
+      {
+        name: 'Rosa Inés Roa Pinilla 4',
+        url:'https://picsum.photos/100',
+        title: 'Fundación Desaparecidos Colombia Huellas de Cristal 4',
+        description: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
+        euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad
+        minim veniam, quis nostrud exerci tation Lorem ipsum dolor sit amet, consectetuer
+        adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+        aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation`
+      },
+      {
+        name: 'Rosa Inés Roa Pinilla 5',
+        url:'https://picsum.photos/200',
+        title: 'Fundación Desaparecidos Colombia Huellas de Cristal 5',
+        description: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
+        euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad
+        minim veniam, quis nostrud exerci tation Lorem ipsum dolor sit amet, consectetuer
+        adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+        aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation`
+      },
+    ]
+  },
+  {
+    age: 2020,
+    users:[
+      {
+        name: 'Rosa Inés Roa Pinilla 6',
+        url:'https://picsum.photos/300',
+        title: 'Fundación Desaparecidos Colombia Huellas de Cristal 6',
+        description: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
+        euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad
+        minim veniam, quis nostrud exerci tation Lorem ipsum dolor sit amet, consectetuer
+        adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+        aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation`
+      },
+      {
+        name: 'Rosa Inés Roa Pinilla 7',
+        url:'https://picsum.photos/400',
+        title: 'Fundación Desaparecidos Colombia Huellas de Cristal 7',
+        description: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
+        euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad
+        minim veniam, quis nostrud exerci tation Lorem ipsum dolor sit amet, consectetuer
+        adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+        aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation`
+      },
+      {
+        name: 'Rosa Inés Roa Pinilla 8',
+        url:'https://picsum.photos/100',
+        title: 'Fundación Desaparecidos Colombia Huellas de Cristal 8',
+        description: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
+        euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad
+        minim veniam, quis nostrud exerci tation Lorem ipsum dolor sit amet, consectetuer
+        adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+        aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation`
+      },
+      {
+        name: 'Rosa Inés Roa Pinilla 9',
+        url:'https://picsum.photos/200',
+        title: 'Fundación Desaparecidos Colombia Huellas de Cristal 9',
+        description: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
+        euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad
+        minim veniam, quis nostrud exerci tation Lorem ipsum dolor sit amet, consectetuer
+        adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+        aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation`
+      }
+    ]
+  },
+  {
+    age: 2019,
+    users:[
+      {
+        name: 'Rosa Inés Roa Pinilla 10',
+        url:'https://picsum.photos/300',
+        title: 'Fundación Desaparecidos Colombia Huellas de Cristal 10',
+        description: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
+        euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad
+        minim veniam, quis nostrud exerci tation Lorem ipsum dolor sit amet, consectetuer
+        adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+        aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation`
+      }
+    ]
+  },
+  {
+    age: 2018,
+    users:[
+      {
+        name: 'Rosa Inés Roa Pinilla 11',
+        url:'https://picsum.photos/400',
+        title: 'Fundación Desaparecidos Colombia Huellas de Cristal 11',
+        description: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
+        euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad
+        minim veniam, quis nostrud exerci tation Lorem ipsum dolor sit amet, consectetuer
+        adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+        aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation`
+      },
+      {
+        name: 'Rosa Inés Roa Pinilla 12',
+        url:'https://picsum.photos/100',
+        title: 'Fundación Desaparecidos Colombia Huellas de Cristal 12',
+        description: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
+        euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad
+        minim veniam, quis nostrud exerci tation Lorem ipsum dolor sit amet, consectetuer
+        adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+        aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation`
+      }
+    ]
+  }
+  ,
+  {
+    age: 2017,
+    users:[
+      {
+        name: 'Rosa Inés Roa Pinilla 13',
+        url:'https://picsum.photos/200',
+        title: 'Fundación Desaparecidos Colombia Huellas de Cristal 13',
+        description: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
+        euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad
+        minim veniam, quis nostrud exerci tation Lorem ipsum dolor sit amet, consectetuer
+        adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+        aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation`
+      },
+      {
+        name: 'Rosa Inés Roa Pinilla 14',
+        url:'https://picsum.photos/300',
+        title: 'Fundación Desaparecidos Colombia Huellas de Cristal 13',
+        description: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
+        euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad
+        minim veniam, quis nostrud exerci tation Lorem ipsum dolor sit amet, consectetuer
+        adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+        aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation`
+      }
+    ]
+  }
+]
--- main.js
+++ main.js
@@ -1,3 +1,120 @@
+import { data } from "./data-info.js";
 
 
+const menu = document.querySelector('.menu-list');
+const usersContainer = document.querySelector('.users-container');
+const modal = document.querySelector('.modal-container');
+let menuItemSelect ;
 
+const toggleModal = (state) => {
+  state ?
+  modal.classList.remove('invisible')
+  : modal.classList.add('invisible');
+}
+
+const handleRenderModal = (data) => {
+  const button = document.createElement('button');
+  const h2 = document.createElement('h2');
+  const p = document.createElement('p');
+
+  button.textContent = 'Cerrar';
+  h2.textContent = data.title;
+  p.textContent = data.description;
+
+  button.addEventListener('click', ()=> toggleModal(false));
+
+  modal.innerHTML = '';
+  modal.appendChild(button);
+  modal.appendChild(h2);
+  modal.appendChild(p);
+
+  toggleModal(true);
+}
+
+const renderDataUser = (data, node) => {
+  /*
+  <article class="user-card">
+    <figure class="user-image">
+      <img src="" alt="imagen random">
+    </figure>
+    <h2 class="user-title">Titulo</h2>
+    <p class="user-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque enim deserunt odio excepturi minus architecto exercitationem incidunt officiis alias, eos sed itaque quis officia fugit modi placeat dolorem hic quos.</p>
+    <a href='#'>Concer más ...>
+  </article>
+  */
+
+  //creamos los nodos
+  const article = document.createElement('article');
+  const figure = document.createElement('figure');
+  const img = document.createElement('img');
+  const h2 = document.createElement('h2');
+  const h3 = document.createElement('h3');
+  const p = document.createElement('p');
+  const div = document.createElement('div');
+  const button = document.createElement('button');
+  const icon = document.createElement('img');
+
+  //completamos la informacion
+  img.src = data.url;
+  h2.textContent = data.name;
+  h3.textContent = data.title;
+  p.textContent = data.description;
+  button.textContent = 'Conoce más';
+  icon.src = 'arrow.png';
+
+  //agregamos el evento de modal al link "conocer más"
+  button.addEventListener('click',() => handleRenderModal(data));
+
+  //anidamos los nodos
+
+  figure.appendChild(img);
+  div.appendChild(button);
+  div.appendChild(icon);
+  article.appendChild(figure);
+  article.appendChild(h2);
+  article.appendChild(h3);
+  article.appendChild(p);
+  article.appendChild(div);
+
+  //agregamos el contenido a contenedor
+  node.appendChild(article);
+}  
+
+const handleMenuItem = (event) => {  
+  const year = event.target.text;
+  const itemParent = event.target.parentNode;
+  
+  //agremos la seleccion al item elegido por el usuario y quitamos la seleccion del item anterioir
+  menuItemSelect.classList.remove('selected');
+  itemParent.classList.add('selected');
+  menuItemSelect = itemParent;
+
+  const dataYear = data.filter(item => item.age == year);
+  usersContainer.innerHTML = '';
+  dataYear[0].users.forEach(item => renderDataUser(item, usersContainer));
+}
+
+const renderMenuItem = (year, node, selected)=> {
+
+  const itemMenu = document.createElement('li');
+  const link = document.createElement('a');
+  link.textContent = year;
+  link.href = '#';
+  link.addEventListener('click', handleMenuItem);
+  itemMenu.classList = 'menu--item';
+
+  //selecionamos el primer item de menu y lo guardamos
+  if(selected) {
+    itemMenu.classList.add('selected');
+    menuItemSelect = itemMenu;
+  }
+
+  itemMenu.appendChild(link);
+  node.appendChild(itemMenu);
+}
+
+//Creamos el menu dinamico a partir de la info
+data.map(item => renderMenuItem(item.age, menu, item.selected));
+
+//creamos el render inicial del primer año
+data[0].users.forEach(item => renderDataUser(item, usersContainer));
